dcd4020de9d4640b0989c9114b984025e3d5f7da
dickyanggairaw <dickyanggairaw@gmail.com>
2021-05-26 17:33:20 +0700

    crd banner

--- server/routers/index.js
+++ server/routers/index.js
@@ -4,6 +4,7 @@ const router = express.Router()
 const productRouter = require('./productRouter')
 const userRouter = require('./userRouter')
 const cartRouter = require('./cartRouter')
+const bannerRouter = require('./bannerRouter')
 
 router.get('/', (req, res) => {
     res.send('Toko Online Ku')
@@ -12,5 +13,6 @@ router.get('/', (req, res) => {
 router.use('/products', productRouter)
 router.use('/users', userRouter)
 router.use('/carts', cartRouter)
+router.use('/banners', bannerRouter)
 
 module.exports = router
