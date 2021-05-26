bbe397d71ff5826b14d01473bb864156e2186d0b
dickyanggairaw <dickyanggairaw@gmail.com>
2021-05-26 11:19:03 +0700

    add table categories and banner

--- server/setup.js
+++ server/setup.js
@@ -23,6 +23,16 @@ const carts = `CREATE TABLE IF NOT EXISTS carts(
     cart_stock INTEGER
 )`
 
+const categories = `CREATE TABLE IF NOT EXISTS categories(
+  id SERIAL PRIMARY KEY,
+  category_name VARCHAR
+)`
+
+const banners = `CREATE TABLE IF NOT EXISTS banners(
+  id SERIAL PRIMARY KEY,
+  banner_address VARCHAR
+)`
+
 const pooling = (data) => {
     return new Promise((resolve, reject) =>{
         pool.query(data, (err, result) => {
@@ -42,5 +52,12 @@ pooling(users)
       return pooling(products)
     })
     .then((data) =>{
+      return pooling(categories)
+    })
+    .then((data) =>{
+      return pooling(banners)
+    })
+    .then((data) =>{
         console.log('create data table success')
+        pool.end()
     })
