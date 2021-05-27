cd899dbad1400d9a4418c82a50dcb76b13c3207c
dickyanggairaw <dickyanggairaw@gmail.com>
2021-05-27 09:11:13 +0700

    index

--- server/seed/index.js
+++ server/seed/index.js
@@ -2,13 +2,13 @@ const seedProducts = require('./products')
 const seedUsers = require('./user')
 const pool = require('../config')
 
-// seedProducts()
-//     .then((data) => {
-//         pool
-//             .query(data)
-//             .then(res => console.log('products success'))
-//             .catch(e => console.error(e.stack))
-//     })
+seedProducts()
+    .then((data) => {
+        pool
+            .query(data)
+            .then(res => console.log('products success'))
+            .catch(e => console.error(e.stack))
+    })
 
 seedUsers()
     .then((data) => {
